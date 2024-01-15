5a0cc2d1083f6ac52dc16c92aadbddf710866fa9
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-01-16 00:36:08 +0100

    Update function.js

--- function.js
+++ function.js
@@ -4,9 +4,10 @@ window.function = async function (address) {
   let adr = address.value;
 
    const response = await fetch
-  (`https://maps.googleapis.com/maps/api/geocode/json?address=${adr}&key=Your API Key`);
+  (`https://randomuser.me/api/?results=3`);
   
   const data = await response.json();
-  return (`${data.results[0].geometry.location.lat}, ${data.results[0].geometry.location.lng}`);
+  const jsonString = JSON.stringify(data); // Convert the object to a JSON string
+    return jsonString;
   
 }
