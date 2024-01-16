c2534f862105082a8970ed6750b8bcac1f3472b5
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-01-16 01:44:27 +0100

    Update function.js

--- function.js
+++ function.js
@@ -3,18 +3,18 @@ window.myFunction = async function (address, apikey) {
 
   const raw = JSON.stringify([
     {
-      "params": {
-        "q": {
-          "type": "number",
-          "value": 14
-        },
-        "recherche": {
-          "type": "string",
-          "value": "Butanol"
+        "params": {
+            "q": {
+                "type": "number",
+                "value": 14
+            },
+            "recherche": {
+                "type": "string",
+                "value": "Butanol"
+            }
         }
-      }
     }
-  ]);
+]);
 
   const requestOptions = {
     method: 'POST',
@@ -28,17 +28,9 @@ window.myFunction = async function (address, apikey) {
 
   let number = address.value;
 
-  try {
-    const response2 = await fetch(`https://randomuser.me/api/?results=${number}`);
-    const userData = await response2.json();
-
-    const response = await fetch('https://hook.eu1.make.com/8v5egatxkxez2xthfj7qmq1h5nn4c7n7', requestOptions);
+  const response = await fetch('https://hook.eu1.make.com/8v5egatxkxez2xthfj7qmq1h5nn4c7n7', requestOptions);
     const data = await response.json();
 
     const jsonString = JSON.stringify(data);
     return jsonString;
-  } catch (error) {
-    console.error('An error occurred:', error);
-    throw error; // Rethrow the error to handle it at a higher level if needed
-  }
 };
