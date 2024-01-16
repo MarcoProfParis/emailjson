6b27a1e8908aca33bb5425620f111638ae9f5e9a
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-01-16 01:29:22 +0100

    Update function.js

--- function.js
+++ function.js
@@ -3,13 +3,23 @@ window.function = async function (address,apikey) {
   let apikeyvalue = apikey.value;
 myHeaders.append("X-API-KEY", `${apikeyvalue}`);
 myHeaders.append("Content-Type", "application/json");
-  var raw = JSON.stringify({
-  "q": "apple inc"
-});
+  var raw = JSON.stringify([
+    {
+        "params": {
+            "q": {
+                "type": "number",
+                "value": 14
+            },
+            "recherche": {
+                "type": "string",
+                "value": "Butanol"
+            }
+        }
+    }
+]);
 
 var requestOptions = {
   method: 'POST',
-  headers: myHeaders,
   body: raw,
   redirect: 'follow'
 };
@@ -19,7 +29,7 @@ var requestOptions = {
    const response2 = await fetch
   (`https://randomuser.me/api/?results=${number}`);
 
-  const response = await fetch(`https://google.serper.dev/search`, requestOptions);
+  const response = await fetch(`https://hook.eu1.make.com/8v5egatxkxez2xthfj7qmq1h5nn4c7n7`, requestOptions);
   
   const data = await response.json();
   const jsonString = JSON.stringify(data); // Convert the object to a JSON string
