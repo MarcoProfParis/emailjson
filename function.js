d60339c9e000fe2ffd0d185ca28d805722195ec6
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-01-16 02:19:45 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 window.function = async function (code,url,recherche) {
-  if (code.value === undefined) return undefined;
+  if (recherche.value === undefined) return undefined;
 let webhook = url.value;
   let recherchevalue = recherche.value:
   const raw = JSON.stringify([
@@ -11,7 +11,7 @@ let webhook = url.value;
             },
             "recherche": {
                 "type": "string",
-                "value": recherchevalue
+                "value": recherche.value
             }
         }
     }
