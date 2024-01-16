71ac6dba7f709ddc30a9d6359518fe1cee89fe1f
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-01-16 01:55:14 +0100

    Update function.js

--- function.js
+++ function.js
@@ -12,7 +12,7 @@ let webhook = webhook.value;
             },
             "recherche": {
                 "type": "string",
-                "value": '${recherche}
+                "value": '${recherche}'
             }
         }
     }
@@ -30,7 +30,7 @@ let webhook = webhook.value;
 
   let number = address.value;
 
-  const response = await fetch('https://hook.eu1.make.com/8v5egatxkxez2xthfj7qmq1h5nn4c7n7', requestOptions);
+  const response = await fetch('${webhook}', requestOptions);
     const data = await response.json();
 
     const jsonString = JSON.stringify(data);
