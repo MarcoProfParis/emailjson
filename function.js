d21a11b6d2e850ad1cd2ee4e937be9030d5baa96
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-01-16 02:17:38 +0100

    Update function.js

--- function.js
+++ function.js
@@ -11,7 +11,7 @@ let webhook = url.value;
             },
             "recherche": {
                 "type": "string",
-                "value": "${recherchevalue}"
+                "value": `${recherchevalue}`
             }
         }
     }
