bdf624138f0ca8529052bfde487e2fecf38fcc8e
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-02-22 21:57:53 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 window.function = async function(json) {
     const obj = JSON.stringify(json);
-    return "Done"+${obj};
+    return "Done";
 };
 window.function2 = async function(json) {
 const obj = JSON.parse(json);
