4febb072ef1dd671e4523c96ce8558c61fefc91f
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-02-22 21:43:37 +0100

    Update function.js

--- function.js
+++ function.js
@@ -12,7 +12,7 @@ window.function = async function(json) {
     
 
  await delay(5000);
-    return "Sending to webhook";
+    return "Sending to webhook ${webhook}";
     const requestOptions = {
         method: 'POST',
         body: JSON.stringify(requestBody),
